dfcd44225f3ce0ab736111e2055e34e9c41cd5d6
Oliver Brown <oliverbrodev@gmail.com>
2024-06-24 16:42:49 -0400

    new commit2

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { FormComponent } from '../form/form.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, FormComponent],
 })
 export class HomePage {
   constructor() {}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,55 @@
 import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import {
+  RouteReuseStrategy,
+  provideRouter,
+  withPreloading,
+  PreloadAllModules,
+} from '@angular/router';
+import {
+  IonicRouteStrategy,
+  provideIonicAngular,
+} from '@ionic/angular/standalone';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
+import { CapacitorUpdater } from '@capgo/capacitor-updater';
+import { App } from '@capacitor/app';
+
+CapacitorUpdater.notifyAppReady();
+
+let data: any ;
+let appInfo: any = {};
+
+App.getInfo().then((info) => {
+  appInfo = info;
+});
+
+App.addListener('appStateChange', async (state) => {
+  alert(JSON.stringify(state));
+  const update = await fetchUpdates();
+
+  if (state.isActive) {
+    data = await CapacitorUpdater.download(update);
+  }
+
+  if (!state.isActive && appInfo.version !== update.version) {
+    try {
+      await CapacitorUpdater.set(data);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+});
+
+async function fetchUpdates() {
+  const response = await fetch("https://raw.githubusercontent.com/oliverdjbrown/capgo-test/main/updates.json");
+
+  const data = await response.json();
+
+  return data;
+}
 
 if (environment.production) {
   enableProdMode();
